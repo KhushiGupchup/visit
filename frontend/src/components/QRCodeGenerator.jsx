9919094e88f4a8e86a7bff12095e781f77f134ed
KhushiGupchup <khushigupchup@gmail.com>
2025-12-23 17:33:32 +0530

    Delete frontend directory

--- frontend/src/components/QRCodeGenerator.jsx
+++
@@ -1,9 +0,0 @@
-import QRCode from "react-qr-code";
-
-export default function QRCodeGenerator({ value }) {
-  return (
-    <div className="bg-white p-4 rounded shadow flex justify-center">
-      <QRCode value={value} size={180} />
-    </div>
-  );
-}
--- frontend/src/components/StatsCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function StatsCard({ title, value }) {
-  return (
-    <div className="bg-white shadow rounded p-5">
-      <h3 className="text-lg font-medium">{title}</h3>
-      <p className="text-3xl font-bold text-blue-600 mt-2">{value}</p>
-    </div>
-  );
-}
