b1099fba297559ab25b26ddf228a7812ff2b00b6
KhushiGupchup <khushigupchup@gmail.com>
2025-12-23 18:17:56 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config(); 
+
+const authRoutes = require("./routes/authRoutes");
+const visitorRoutes = require("./routes/visitorRoutes");
+const admin = require("./routes/admin");
+const employee = require("./routes/employee");
+const security = require("./routes/security")
+const otpRoutes = require("./routes/otpRoutes"); // Adjust path if needed
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/visitor", visitorRoutes);
+app.use("/api/admin", admin);
+app.use("/api/employee",employee)
+app.use("/api/security",security);
+
+
+app.use("/api", otpRoutes);
+
+
+
+app.listen(5000, () => console.log("Server running on port 5000"));
