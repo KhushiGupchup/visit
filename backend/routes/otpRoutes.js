053fd857c0e138f8e80078ca035cb0ec5dafc45a
KhushiGupchup <khushigupchup@gmail.com>
2025-12-25 00:56:09 +0530

    Update otpRoutes.js

--- backend/routes/otpRoutes.js
+++ backend/routes/otpRoutes.js
@@ -4,20 +4,20 @@ const sendEmail = require("../utils/sendEmail");
 
 const otpStore = {};
 router.post("/send-otp", async (req, res) => {
-    const { email } = req.body;
+    const { email } = req.body;//take email first
     if (!email) {
         return res.status(400).json({ message: "Email is required" });
     }
 
-    //  6-digit OTP
+    //  6-digit OTP be randomly generated
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
-    // Store the OTP for 5 minutes
+    // Store that randomly generated OTP for 5 minutes
     otpStore[email] = {
         otp,
         expires: Date.now() + 5 * 60 * 1000
     };
-
+//send otp to taken email
     await sendEmail(
         email,
         "Your OTP Code",
