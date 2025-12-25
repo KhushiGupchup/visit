e6cbb6ab666c63e7a619fedf57cee42a86a1ecea
KhushiGupchup <khushigupchup@gmail.com>
2025-12-25 12:15:48 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connectDB = async () => {
+async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(" MongoDB Atlas Connected");
-  } catch (error) {
-    console.error(" MongoDB connection error:", error);
-    process.exit(1);
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
   }
-};
+}
 
 module.exports = connectDB;
