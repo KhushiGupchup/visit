f69c0b17634fab1c3fd7b666303873c8f718d355
KhushiGupchup <khushigupchup@gmail.com>
2025-12-26 00:43:14 +0530

    Update generatePDF.js

--- backend/utils/generatePDF.js
+++ backend/utils/generatePDF.js
@@ -1,24 +1,25 @@
-const PDFDocument = require("pdfkit");
+const PDFDocument = require("pdfkit");//to create pdf
 const fs = require("fs");
 const path = require("path");
 
+//visitor details and qr image and where to store
 async function generatePDF(visitor, qrBase64, outputPath) {
   return new Promise((resolve, reject) => {
-    const doc = new PDFDocument({ size: "A4", margin: 50 });
-    const dir = path.dirname(outputPath);
-    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    const doc = new PDFDocument({ size: "A4", margin: 50 });//make document 
+    const dir = path.dirname(outputPath);//save in given path
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });//if path not their then create
 
-    const stream = fs.createWriteStream(outputPath);
+    const stream = fs.createWriteStream(outputPath);//send content to file
     doc.pipe(stream);
 
-    // Header
+    // Title of document or pdf
     doc
       .fontSize(24)
       .fillColor("#008080")
       .text("Visitor Pass", { align: "center", underline: true });
     doc.moveDown(1.5);
 
-    // Visitor Info Box
+    // show all Visitor Info 
     doc
       .fillColor("#000")
       .fontSize(14)
@@ -32,7 +33,7 @@ async function generatePDF(visitor, qrBase64, outputPath) {
       .text(`Purpose: ${visitor.purpose}`);
     doc.moveDown(1);
 
-    // Host Info Box
+    // Host Info and date ,time
     doc
       .fontSize(14)
       .fillColor("#000")
@@ -47,10 +48,10 @@ async function generatePDF(visitor, qrBase64, outputPath) {
       );
     doc.moveDown(2);
 
-    // QR Code
+    // QR Code 
     const base64Data = qrBase64.replace(/^data:image\/png;base64,/, "");
-    const tempQRPath = path.join(__dirname, `tmp_qr_${visitor._id}.png`);
-    fs.writeFileSync(tempQRPath, base64Data, "base64");
+    const tempQRPath = path.join(__dirname, `tmp_qr_${visitor._id}.png`);//temporary image file of qr
+    fs.writeFileSync(tempQRPath, base64Data, "base64");//add it
 
     const qrX = (doc.page.width - 150) / 2; // center QR
     doc.image(tempQRPath, qrX, doc.y, { width: 150 });
@@ -59,10 +60,10 @@ async function generatePDF(visitor, qrBase64, outputPath) {
    
 
 
-    doc.end();
+    doc.end();// done all contents of files
 
     stream.on("finish", () => {
-      fs.unlinkSync(tempQRPath);
+      fs.unlinkSync(tempQRPath);//remove temporary qr 
       resolve(outputPath);
     });
 
@@ -71,3 +72,4 @@ async function generatePDF(visitor, qrBase64, outputPath) {
 }
 
 module.exports = {generatePDF};
+
