5f1e71ab41eb75d6e1c0abd823bc0ae9c7957774
KhushiGupchup <khushigupchup@gmail.com>
2025-12-26 00:49:49 +0530

    Update generateQR.js

--- backend/utils/generateQR.js
+++ backend/utils/generateQR.js
@@ -1,9 +1,10 @@
-const QRCode = require("qrcode");
+const QRCode = require("qrcode");// qrcode library to create QR
 
-
+//data-text which will go in qr or show that text as QR
 async function generateQRBase64(data) {
-  return await QRCode.toDataURL(data);
+  return await QRCode.toDataURL(data);//make QR 
 }
 
 module.exports = { generateQRBase64 };
+
 
