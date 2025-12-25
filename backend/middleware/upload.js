7619eb2e1d2234c76fb2b1ea30ab79050f0d289a
KhushiGupchup <khushigupchup@gmail.com>
2025-12-26 00:14:24 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,24 +1,25 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require("multer");//this is use to store files
+const path = require("path");//here where to store
 
-// Set storage engine
+//destination will be uploads folder 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + file.originalname.replace(/\s+/g, "_");
-    cb(null, uniqueName);
+    const uniqueName = Date.now() + "-" + file.originalname.replace(/\s+/g, "_");//give the name to that uploaded file
+    cb(null, uniqueName);//save the given name
   },
 });
 
-// File filter (optional: allow only images)
+//check image
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = /jpeg|jpg|png|gif/;
-  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = allowedTypes.test(file.mimetype);
-  if (mimetype && extname) return cb(null, true);
-  cb("Error: Images only!");
+  const allowedTypes = /jpeg|jpg|png|gif/;//allowed extension
+  const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());//check extension of file
+  const mimetype = allowedTypes.test(file.mimetype);//check if actual image type
+  if (mimetype && extname) return cb(null, true);//valid image and type then ok 
+  cb("Error: Images only!");//if mismatched show error
 };
 
 const upload = multer({ storage, fileFilter });
 
 module.exports = upload;
+
