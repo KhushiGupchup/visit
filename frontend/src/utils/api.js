912474bfd4cdea18e74dec5809ff23d30917516f
KhushiGupchup <khushigupchup@gmail.com>
2025-12-26 16:24:37 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance for backend API
 const api = axios.create({
-  baseURL: "https://visit-1-ren0.onrender.com/api", // your backend base URL
+  baseURL: "https://visit-1-ren0.onrender.com/api", // backend api
 });
 
 // Add JWT token to every request if available
@@ -15,4 +15,5 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
 
