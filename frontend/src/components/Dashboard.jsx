9f7dcd6d935065e96e8207b0f469c067da5d6e97
KhushiGupchup <khushigupchup@gmail.com>
2025-12-26 13:58:07 +0530

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -6,30 +6,30 @@ import api from "../utils/api";
 import { AuthContext } from "../context/AuthContext";
 
 export default function Dashboard() {
-  const { user, logout } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);//user data and logout
   const [stats, setStats] = useState({
     totalEmployees: 0,
     totalVisitors: 0,
     pendingVisitors: 0,
-  });
+  });//set count at 0 first
 
   useEffect(() => {
     async function fetchStats() {
       try {
-        const token = localStorage.getItem("token");
+        const token = localStorage.getItem("token");//get token 
         if (!token) return;
-
+        // call backend api and token 
         const res = await api.get("/admin/dashboard", {
           headers: { Authorization: `Bearer ${token}` },
         });
-        setStats(res.data);
+        setStats(res.data);//then add the count
       } catch (err) {
         console.error(err);
         if (err.response?.status === 401) logout();
       }
     }
 
-    if (user) fetchStats();
+    if (user) fetchStats();//take count 
   }, [user, logout]);
 
   if (!user) return null;
@@ -45,16 +45,19 @@ export default function Dashboard() {
           
         {/* Stats */}
         <div className="p-6 md:p-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* Total Employee *}
           <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
             <h3 className="text-lg font-bold text-gray-700">Total Employees</h3>
             <p className="text-4xl font-bold mt-4 text-blue-600">{stats.totalEmployees}</p>
           </div>
-
+          
+          {/* Total Visitors *}
           <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
             <h3 className="text-lg font-bold text-gray-700">Total Visitors</h3>
             <p className="text-4xl font-bold mt-4 text-teal-600">{stats.totalVisitors}</p>
           </div>
 
+           {/* Pending Visitors *}
           <div className="bg-white p-6 rounded-xl shadow hover:shadow-md transition">
             <h3 className="text-lg font-bold text-gray-700">Pending Visitors</h3>
             <p className="text-4xl font-bold mt-4 text-yellow-500">{stats.pendingVisitors}</p>
@@ -70,3 +73,4 @@ export default function Dashboard() {
     </div>
   );
 }
+
