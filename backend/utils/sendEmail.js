acd972ff6f94825428ac9873d7d7fbdc31f37f97
KhushiGupchup <khushigupchup@gmail.com>
2026-01-08 16:45:38 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,32 +1,22 @@
 const emailjs = require("@emailjs/nodejs");
 
-// Initialize EmailJS client with public and private key
-const client = emailjs.client.init({
-  publicKey: process.env.EMAILJS_PUBLIC_KEY,
-  privateKey: process.env.EMAILJS_PRIVATE_KEY,
-});
-
 const sendEmail = async (toEmail, templateParams = {}, attachments = []) => {
   try {
-    const response = await client.messages.send({
-      service_id: process.env.EMAILJS_SERVICE_ID,
-      template_id: process.env.EMAILJS_TEMPLATE_ID,
-      template_params: {
+    await emailjs.send(
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
+      {
         ...templateParams,
-        to_email: toEmail, // pass recipient
+        to_email: toEmail,
+        attachments, // [{ name: "file.png", data: base64 }]
       },
-      attachments: attachments.map(att => ({
-        name: att.name,
-        data: att.data, // base64 string
-      })),
-    });
-
-    console.log("Email sent to:", toEmail, "Response:", response);
-    return response;
+      process.env.EMAILJS_PRIVATE_KEY
+    );
+    console.log("Email sent to:", toEmail);
   } catch (err) {
     console.error("EmailJS send error:", err);
     throw err;
   }
 };
 
-module.exports = sendEmail;
+module.exports = sendEmail; here do
