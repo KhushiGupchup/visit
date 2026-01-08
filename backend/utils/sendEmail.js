6357d5620b07c4d094eb3d71a456f5934817571c
KhushiGupchup <khushigupchup@gmail.com>
2026-01-08 17:13:25 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,5 +1,7 @@
 const emailjs = require("@emailjs/nodejs");
 require("dotenv").config();
+console.log("EMAILJS_PUBLIC_KEY:", process.env.EMAILJS_PUBLIC_KEY);
+
 
 if (!process.env.EMAILJS_SERVICE_ID || !process.env.EMAILJS_TEMPLATE_ID || !process.env.EMAILJS_PUBLIC_KEY) {
   throw new Error("EmailJS environment variables are missing! Check .env file");
