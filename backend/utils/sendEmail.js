8cf0af17fe2f9d2e606ab2af41532f81cbf27c6d
KhushiGupchup <khushigupchup@gmail.com>
2026-01-08 13:59:07 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,28 +1,28 @@
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
 const sendEmail = async (to, subject, html, attachments = []) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
     await transporter.sendMail({
       from: process.env.FROM_EMAIL,
       to,
       subject,
       html,
-      attachments, // <-- attachments array added
+      attachments,
     });
-
     console.log("Email sent to:", to);
   } catch (error) {
-    console.log("Email Error:", error.message);
+    console.error("Email Error:", error);
+    throw error;
   }
 };
 
