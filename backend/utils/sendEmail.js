0f8a9fb6790ccaf5b9097578d406f0e9e43a966d
KhushiGupchup <khushigupchup@gmail.com>
2026-01-08 16:51:53 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,17 +1,23 @@
 const emailjs = require("@emailjs/nodejs");
+require("dotenv").config(); // Make sure .env variables are loaded
 
 const sendEmail = async (toEmail, templateParams = {}, attachments = []) => {
   try {
+    if (!process.env.EMAILJS_SERVICE_ID || !process.env.EMAILJS_TEMPLATE_ID || !process.env.EMAILJS_PUBLIC_KEY) {
+      throw new Error("EmailJS environment variables missing!");
+    }
+
     await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID,
-      process.env.EMAILJS_TEMPLATE_ID,
+      process.env.EMAILJS_SERVICE_ID,   // Service ID
+      process.env.EMAILJS_TEMPLATE_ID,  // Template ID
       {
         ...templateParams,
         to_email: toEmail,
         attachments, // [{ name: "file.png", data: base64 }]
       },
-      process.env.EMAILJS_PRIVATE_KEY
+      process.env.EMAILJS_PUBLIC_KEY    // <-- Use PUBLIC key here, NOT private key
     );
+
     console.log("Email sent to:", toEmail);
   } catch (err) {
     console.error("EmailJS send error:", err);
@@ -19,4 +25,4 @@ const sendEmail = async (toEmail, templateParams = {}, attachments = []) => {
   }
 };
 
-module.exports = sendEmail; 
+module.exports = sendEmail;
